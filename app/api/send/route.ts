777ebaef5a6c0e2edd7971be39f6ad3c8c454287
julialimp <julia.limp@hotmail.com>
2026-08-04 01:42:12 -0300

    adding attachment

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,11 +1,36 @@
 import { Resend } from "resend";
 import { BillingFailureEmail } from "@/emails/BillingFailure";
+import fs from "fs";
+import path from "path";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-const { data, error } = await resend.emails.send({
-  from: 'Acme <onboarding@resend.dev>',
-  to: ['julia.limp@hotmail.com'],
-  subject: 'Payment failed',
-  react: BillingFailureEmail({ name: 'Julia' }),
-});
+export async function GET() {
+  const invoicePath = path.join(
+    process.cwd(),
+    "attachments",
+    "test-attachment.txt",
+  );
+
+  const invoiceBuffer = fs.readFileSync(invoicePath);
+
+  const { data, error } = await resend.emails.send({
+    from: "Acme <onboarding@resend.dev>",
+    to: ["julia.limp@hotmail.com"],
+    subject: "Payment failed",
+    react: BillingFailureEmail({ name: "Julia" }),
+    attachments: [
+      {
+        filename: "test-attachment.txt",
+        content: invoiceBuffer,
+      },
+    ],
+  });
+
+  if (error) {
+    console.error("Error sending email:", error);
+    return new Response("Error sending email", { status: 500 });
+  }
+
+  return new Response("Email sent successfully", { status: 200 });
+}
